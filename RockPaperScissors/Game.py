388d62ced3f11211a7192aa295414e89bbd56c14
MustafaYagizKaragoz <yagizkaragoz7003@gmail.com>
2024-11-23 17:00:55 +0300

    Finish!

--- RockPaperScissors/Game.py
+++ RockPaperScissors/Game.py
@@ -1,14 +1,21 @@
 from player import Player
 from GameManager import GameManager
 
-p1=Player('Mustafa',is_human=True)
-p2=Player("PC",is_human=False)
-gm=GameManager(p1,p2)
 
-playable=True
-while playable:
-    choice_1=p1.make_choise()
-    choice_2=p2.make_choise()
-    gm.who_won()
-    gm.is_finish()
+class Game:
+    def create_player(self,player1_name,player1_is_human,player2_name,player2_is_human):
+        self.player1=Player(player1_name,player1_is_human)
+        self.player2=Player(player2_name,player2_is_human)
+        self.gm=GameManager(self.player1,self.player2)
 
+    def play_game(self):
+        while True:
+            choice_1=self.player1.make_choise()
+            choice_2=self.player2.make_choise()
+            self.gm.who_won()
+            self.gm.is_finish()
+
+if __name__=="__main__":
+    game=Game()
+    game.create_player("Ecem",True,"Pc",True)
+    game.play_game()
--- RockPaperScissors/GameManager.py
+++ RockPaperScissors/GameManager.py
@@ -1,26 +1,31 @@
 class GameManager:
-    def __init__(self,player1,player2):
-        self.player1=player1
-        self.player2=player2
 
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
 
     def is_finish(self):
-        if self.player1.get_score()==3:
-            print(f"-------------------{self.player1.get_name()} won!!!!-----------------")
+        if self.player1.get_score() == 3:
+            print(
+                f"-------------------{self.player1.get_name()} won!!!!-----------------"
+            )
             exit()
-        elif self.player2.get_score()==3:
-            print(f"-------------------{self.player2.get_name()} won!!!!-----------------")
+        elif self.player2.get_score() == 3:
+            print(
+                f"-------------------{self.player2.get_name()} won!!!!-----------------"
+            )
             exit()
+
     def who_won(self):
-        choice_1=self.player1.get_choice()
-        choice_2=self.player2.get_choice()
-        if choice_1==choice_2:
+        choice_1 = self.player1.get_choice()
+        choice_2 = self.player2.get_choice()
+        if choice_1 == choice_2:
             print("Draw")
         elif (choice_1 == "rock" and choice_2 == "scissors") or \
                 (choice_1 == "paper" and choice_2 == "rock") or \
                 (choice_1 == "scissors" and choice_2 == "paper"):
-                self.player1.update_score()
-                print(self.player1.get_name() +" won")
+            self.player1.update_score()
+            print(self.player1.get_name() + " won")
         else:
             self.player2.update_score()
             print(self.player2.get_name() + " won")
