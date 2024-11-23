19526147ab5173f53cb42089d3e4ced7405dda8f
MustafaYagizKaragoz <yagizkaragoz7003@gmail.com>
2024-11-23 16:46:52 +0300

    Finishhgit status!

--- RockPaperScissors/player.py
+++ RockPaperScissors/player.py
@@ -1,28 +1,38 @@
 import random
+
+
 class Player:
-    def __init__(self,name,is_human):
-        self.__score=0
-        self.current_choice=""
-        self.__name=name
-        self.__is_human=is_human
-        self.__list=["rock","paper","scissors"]
+
+    def __init__(self, name, is_human):
+        self.__score = 0
+        self.current_choice = ""
+        self.__name = name
+        self.__is_human = is_human
+        self.__list = ["rock", "paper", "scissors"]
+
     def make_choise(self):
         if self.__is_human:
-            self.current_choice=input(f"{self.__name}, make your choice rock,paper,scissors: ").lower()
+            self.current_choice = input(
+                f"{self.__name}, make your choice rock,paper,scissors: "
+            ).lower()
             while self.current_choice not in self.__list:
                 print("Invalid choice please try again")
-                self.current_choice=input(f"{self.__name}, make your choice rock,paper,scissors: ").lower()
+                self.current_choice = input(
+                    f"{self.__name}, make your choice rock,paper,scissors: "
+                ).lower()
         else:
-            self.current_choice=random.choice(self.__list)
+            self.current_choice = random.choice(self.__list)
             print(f"{self.__name} chose {self.current_choice}")
         return self.current_choice
-    def  update_score(self):
-        self.__score+=1
-    def reset_score(self):
-        self.__score=0
+
+    def update_score(self):
+        self.__score += 1
+
     def get_choice(self):
         return self.current_choice
+
     def get_name(self):
         return self.__name
+
     def get_score(self):
         return self.__score
