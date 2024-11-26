7ead4e34739e7363f6ed80cf28706e2de20f70b9
MustafaYagizKaragoz <yagizkaragoz7003@gmail.com>
2024-11-27 00:31:21 +0300

    We can move paddles

--- PingPong/game.py
+++ PingPong/game.py
@@ -3,6 +3,7 @@ from paddle import Paddle
 
 pygame.init()
 BLACK = (0, 0, 0)
+WEIGHT = (255, 255, 255)
 WIDTH, HEIGHT = 700, 500
 PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
 class Game:
@@ -22,16 +23,27 @@ class Game:
         while self.run:
             self.clock.tick(self.FPS)
             self.draw(self.display, [self.left_paddle, self.right_paddle])
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_w] and self.left_paddle.y - self.left_paddle.velocity >= 0:
+                    self.left_paddle.move("UP")
+            if keys[pygame.K_s] and self.left_paddle.y + self.left_paddle.velocity <= HEIGHT - PADDLE_HEIGHT:
+                    self.left_paddle.move("DOWN")
+            if keys[pygame.K_UP] and self.right_paddle.y - self.right_paddle.velocity >= 0:
+                    self.right_paddle.move("UP")
+            if keys[pygame.K_DOWN] and self.right_paddle.y + self.right_paddle.velocity <= HEIGHT - PADDLE_HEIGHT:
+                    self.right_paddle.move("DOWN")
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.run = False
                     break
-        pygame.quit()
-
     def draw(self, display,paddles):
         display.fill(BLACK)
         for paddle in paddles:
             paddle.draw(display)
+            
+        for i in range(10,HEIGHT,HEIGHT//20):
+            if i % 2 == 0:
+                pygame.draw.rect(display, WEIGHT, (WIDTH//2 - 2, i, 4, HEIGHT//20))
         pygame.display.update()
 
 
--- PingPong/paddle.py
+++ PingPong/paddle.py
@@ -8,7 +8,13 @@ class Paddle:
         self.width = width
         self.height = height
         self.COLOR = (255, 255, 255)
+        self.velocity = 4
 
     def draw(self, display):
         pygame.draw.rect(display, self.COLOR,
                               (self.x, self.y, self.width, self.height))
+    def move(self, direction):
+        if direction == "UP":
+            self.y -= self.velocity
+        elif direction == "DOWN":
+            self.y += self.velocity
